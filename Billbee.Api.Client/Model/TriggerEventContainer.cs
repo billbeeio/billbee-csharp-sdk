443ce63246747f8d9043956c3e1ceb54a491d464
Aljoscha Jürgen Knaust <aljoscha@billbee.de>
2019-10-02 14:37:13 +0200

    Added possibility to trigger an event via API

---
+++ Billbee.Api.Client/Model/TriggerEventContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billbee.Api.Client.Model
+{
+    public class TriggerEventContainer
+    {
+        /// <summary>Name of the event</summary>
+        public string Name { get; set; }
+
+        /// <summary>The delay in minutes until the rule is executed</summary>
+        public uint DelayInMinutes { get; set; } = 0;
+    }
+}
