e7011f5ed12a96fcab7d122822a56bb4c210c4ce
Benjamin Gottschalk <benjamin@billbee.io>
2022-08-11 13:58:56 +0200

    fix broken build #66

---
+++ Billbee.Api.Client/Model/LayoutTemplate.cs
@@ -0,0 +1,11 @@
+﻿using Billbee.Api.Client.Enums;
+
+namespace Billbee.Api.Client.Model
+{
+    public class LayoutTemplate
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public ReportTemplates Type { get; set; }
+    }
+}
